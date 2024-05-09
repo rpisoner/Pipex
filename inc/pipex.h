80dc65629a93e897563b7208b83c7738463a2e4e
rpisoner <rpisoner@student.42madrid.com>
2024-05-09 11:08:04 +0200

    first

---
+++ inc/pipex.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rpisoner <rpisoner@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/01 12:20:17 by rpisoner          #+#    #+#             */
+/*   Updated: 2024/05/01 12:36:27 by rpisoner         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_H
+# define PIPEX_H
+# include <fcntl.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <string.h>
+# include <sys/types.h>
+# include <sys/wait.h>
+
+# include <stdio.h>
+
+#endif 
